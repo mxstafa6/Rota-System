3d92abc9ca76eb687160c04c951160ec9c77d29f
mxstafa6 <cetin.musti@yahoo.com>
2024-04-13 14:58:07 +0100

    T the goat

--- ObjectCreation.py
+++ ObjectCreation.py
@@ -3,13 +3,13 @@ import sqlite3
 class Employee:
     def __init__(self, firstname, lastname, age, role, gender):
         # Initialize Employee object with provided attributes
-        self.firstname = firstname.strip()
+        self.firstname = firstname.strip().capital
         self.lastname = lastname.strip()
         self.age = age
         self.role = role
         self.gender = gender
 
-def Serialize(x,k):
+def Serialize(dict,list):
     # Retrieve employee data from the database
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
@@ -18,8 +18,8 @@ def Serialize(x,k):
     # Organize employee data as objects, stored in the dictionary by key
     for data in employees_data:
         firstname, lastname, age, role, gender, key= data
-        k.append(key)
+        list.append(key)
         employee = Employee(firstname, lastname, age, role, gender)
-        x.setdefault(key, []).append(employee)
+        dict.setdefault(key, []).append(employee)
     conn.close()
 
