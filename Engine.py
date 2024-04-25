1d6a83739c387b0ada315bda80f7541391dcc214
mxstafa6 <cetin.musti@yahoo.com>
2024-04-25 10:19:21 +0100

    s

--- Engine.py
+++ Engine.py
@@ -7,3 +7,11 @@ thursday={}
 friday={}
 saturday={}
 sunday={}
+
+class Rota()
+conn = sqlite3.connect('data.db')  # Connect to the SQLite database
+cursor = conn.cursor()
+# Check if the restaurant name already exists in the database
+cursor.execute("SELECT restaurantName FROM restaurant_data WHERE restaurantName=?")
+existing_name = cursor.fetchone()
+conn.close()
